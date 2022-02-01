84064e00d7e00360fd760c0a7c0b064acaa5ac6c
edkrueger <edkrueger@gmail.com>
2022-02-01 16:13:12 -0600

    Solve cigar_party.

--- logic-1.py
+++ logic-1.py
@@ -11,7 +11,10 @@ def cigar_party(cigars, is_weekend):
     cigar_party(50, False) → True
     cigar_party(70, True) → True
     """
-    return
+
+    if is_weekend:
+        return cigars >=40
+    return 40 <= cigars <= 60
 
 
 def date_fashion(you, date):
